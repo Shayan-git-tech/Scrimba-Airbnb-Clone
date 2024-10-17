bd805aceb1bd65ec6a2b906af84b98f7f86dbefb
Shayan-git-tech <shayankhan12340@gmail.com>
2024-10-18 01:58:58 +0500

    Updated Image Src

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
       <header>
       <div className='logo-container'>
-      <img src="/Images/Color-Airbnb-Logo.jpg" alt="Logo" className='logo'/>
+      <img src={`${process.env.PUBLIC_URL}/Images/Color-Airbnb-Logo.jpg`} alt="Logo" className='logo'/>
       </div>
     </header>
     );
